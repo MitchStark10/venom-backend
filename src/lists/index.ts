3268e739cba049b0ada770a5747ce49033c3551e
Mitch Stark <mitchstark10@gmail.com>
2024-02-18 19:40:52 -0600

    fix order by statement

--- src/lists/index.ts
+++ src/lists/index.ts
@@ -20,7 +20,7 @@ app.get("/", async (req, res) => {
     include: {
       tasks: {
         orderBy: {
-          listViewOrder: "asc",
+          timeViewOrder: "asc",
         },
         where: {
           isCompleted: false,
