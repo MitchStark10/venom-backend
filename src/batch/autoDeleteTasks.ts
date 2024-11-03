0f796c57de8dcbb48752aaee5c8a376ba41c470e
Mitch Stark <mitchstark10@gmail.com>
2024-11-02 21:42:03 -0500

    syntax fixes

--- src/batch/autoDeleteTasks.ts
+++ src/batch/autoDeleteTasks.ts
@@ -1,45 +1,56 @@
-import { extendedPrisma } from '../../lib/extendedPrisma';
-import { AutoDeleteTaskOptions } from '@prisma/client';
+import { AutoDeleteTasksOptions } from "@prisma/client";
+import { extendedPrisma } from "../lib/extendedPrisma";
+import { getDateWithOffset } from "../lib/getTomorrowDate";
 
 const OFFSET_MAP = {
-    AutoDeleteTaskOptions.ONE_WEEK: -7,
-    AutoDeleteTaskOptions.TWO_WEEKS: -14
-    AutoDeleteTaskOptions.ONE_MONTH: -30
+  [AutoDeleteTasksOptions.ONE_WEEK]: -7,
+  [AutoDeleteTasksOptions.TWO_WEEKS]: -14,
+  [AutoDeleteTasksOptions.ONE_MONTH]: -30,
+};
+
+interface Params {
+  isDryRun?: boolean;
 }
 
-export const autoDeleteTasks = async () => {
-    const users = await extendedPrisma.user.find({
-        where: {
-            autoDeleteTasks: {
-                not: AutoDeleteTaskOptions.NEVER // TODO: Verify this condition
-            },
-        }
-    });
-    
-    for (const user of users) {
-        const dateToCheck = getDateWithOffset(OFFSET_MAP[user.autoDeleteTasks]);
-    
-        const taskFilter = {
-            userId: user.id,
-            completedDate: { // TODO: Need to run the migration for this
-                lte: dateToCheck,
-            },
-            isCompleted: true
-        };
-        
-        if (isDryRun) {
-            const tasks = await extendedPrisma.task.find({
-                where: taskFilter
-            });
-            
-            for (const task of tasks) {
-                console.log('Task eligible for deletion', task);
-            }
-        } else {
-            const deletedTasks = await extendedPrisma.task.delete({
-                where: taskfilter
-            });       
-            console.log("Deletion operation completed", deletedTasks)
-        }
+export const autoDeleteTasks = async ({ isDryRun }: Params) => {
+  const users = await extendedPrisma.user.findMany({
+    where: {
+      autoDeleteTasks: {
+        not: AutoDeleteTasksOptions.NEVER,
+      },
+    },
+  });
+
+  for (const user of users) {
+    if (user.autoDeleteTasks === AutoDeleteTasksOptions.NEVER) {
+      console.error("Invalid autoDeleteTasks value", user.autoDeleteTasks);
+      continue;
     }
+
+    const dateToCheck = getDateWithOffset(OFFSET_MAP[user.autoDeleteTasks]);
+
+    const taskFilter = {
+      userId: user.id,
+      completedDate: {
+        // TODO: Need to run the migration for this
+        lte: dateToCheck,
+      },
+      isCompleted: true,
+    };
+
+    if (isDryRun) {
+      const tasks = await extendedPrisma.task.findMany({
+        where: taskFilter,
+      });
+
+      for (const task of tasks) {
+        console.log("Task eligible for deletion", task);
+      }
+    } else {
+      const deletedTasks = await extendedPrisma.task.deleteMany({
+        where: taskFilter,
+      });
+      console.log("Deletion operation completed", deletedTasks);
+    }
+  }
 };
